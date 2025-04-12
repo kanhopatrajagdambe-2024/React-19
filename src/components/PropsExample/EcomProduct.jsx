ec904b5a04e465c5b932f797762973a5e8027f16
kanhopatrajagdambe-2024 <reachkanhopatrajagdambe@gmail.com>
2025-04-12 12:51:43 +0530

    added conditional rendering

--- src/components/PropsExample/EcomProduct.jsx
+++ src/components/PropsExample/EcomProduct.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 export default function EcomProduct(props) {
   const {carddata} = props
-  console.log("data", carddata)
   return (
   <>
  <div className="mx-4 w-80 h-[500px] bg-white shadow-lg rounded-lg overflow-hidden flex flex-col">
@@ -20,8 +19,12 @@ export default function EcomProduct(props) {
     </p>
   </div>
   <div className="px-6 pt-4 pb-2" >
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${carddata.price}</span>
-    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{carddata.rating.rate}</span>
+    <span className="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+      <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Buy Now</button>
+    </span>
+    <span className={carddata.rating.rate > 4 ? "bg-green-500": "bg-yellow-500"} >
+      Rating: {carddata.rating.rate}
+      </span>
   </div>
   </div>
   </>
