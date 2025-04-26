95d08babe942d488308e2b8c0592f7cb0d1a15df
kanhopatrajagdambe-2024 <reachkanhopatrajagdambe@gmail.com>
2025-04-26 17:23:58 +0530

    Implimented Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import StateExample , {Sibling, StateExample2} from './components/Hooks/StateExa
 import DerivedState from './components/Hooks/DerivedState'
 import StateUpLifting from './components/Hooks/StateUpLifting'
 import DerivedStateExample from './components/Hooks/DerivedStateExample'
+import ReactIconExample from './components/Hooks/ReactIconExample'
+import Todo from './components/projects/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <DerivedStateExample />
+   <Todo />
     </>
   ) 
 }
---
+++ src/components/Hooks/ReactIconExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { IoPeopleOutline } from "react-icons/io5";
+export default function ReactIconExample() {
+  return (
+    <div>
+        <span>person</span>
+        <IoPeopleOutline />
+
+    </div>
+  )
+}
